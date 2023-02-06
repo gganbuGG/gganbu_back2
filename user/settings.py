d13b09bf576a1f5764e5296d5f400decb0c44e6b
jimin Kim <jimin713@naver.com>
2023-02-06 20:55:03 +0900

    시크릿키 분리

--- user/settings.py
+++ user/settings.py
@@ -9,21 +9,36 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os,json
-from django.core.exceptions import ImproperlyConfigured
-from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from pathlib import Path
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+secret_file = os.path.join(BASE_DIR, 'secrets.json')  # secrets.json 파일 위치를 명시
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d1b$8f+$r=ew0u@ofw3@!e&s(e5@3k94u+c&erxj)k&5l!02oj'
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
 
-# SECURITY WARNING: don't run with debug turned on in production!
+def get_secret(setting):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+
+
+
+
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
