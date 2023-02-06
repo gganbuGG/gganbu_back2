d33bf798368f99e796cabb0781085da6787676c4
jimin Kim <jimin713@naver.com>
2023-02-06 21:03:23 +0900

    secret key

--- config/settings.py
+++ config/settings.py
@@ -11,16 +11,27 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os,json
 from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+secret_file = os.path.join(BASE_DIR, 'secret.json')  # secrets.json 파일 위치를 명시
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4xueee$g^(u0^kaf*ek)^8co%wcy(chwnh(^x^g)5107#gn2@c'
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
