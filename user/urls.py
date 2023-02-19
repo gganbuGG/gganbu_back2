5ce15d93fc65426a1e2d64c9f79cf7996ca60ee8
jimin Kim <jimin713@naver.com>
2023-02-19 21:19:12 +0900

    riot.txt

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
 from .views import UserAPI,usersAPI,statAPI
+from user import views
 urlpatterns=[
     path('<str:sname>/', UserAPI.as_view()),
     path('info/<str:sname>/',usersAPI.as_view()),
     path('info/<str:sname>/stat',statAPI.as_view()),
-
+    path('',views.info ),
+    path('riot/riot.txt/', views.riot),
 ]
